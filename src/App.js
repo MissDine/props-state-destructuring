a2f0919becb8802fe9fafb3c40796fe3c6e920cc
MissDine <enidnyatichi1212@gmail.com>
2022-05-28 18:09:27 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+import './App.css';
+import Greetings from './components/Greetings';
+
+function App() {
+  return (
+    <Greetings name ="Dine"/>
+  );
+}
+
+export default App;
---
+++ src/components/Greetings.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Greetings = ({name}) => <div>Hello {name}</div>
+
+export default Greetings
